6d24ce731540d67099a3e9a90cb538991cc3376a
JuanDiegoAnteNovoa <juan.ante96@gmail.com>
2023-07-12 17:42:25 -0500

    subiendo api_gateway

---
+++ mc_api_gateway_inventario/main.py
@@ -0,0 +1,144 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import json
+from waitress import serve
+import datetime
+import requests
+import re
+from flask_jwt_extended import create_access_token, verify_jwt_in_request
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+cors = CORS(app)
+
+app.config["JWT_SECRET_KEY"] = "super-secret" # Cambiar por el que se conveniente
+jwt = JWTManager(app)
+
+
+@app.route("/login", methods=["POST"])
+def create_token():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url=dataConfig["url-backend-security"]+'/usuarios/validar'
+    response = requests.post(url, json=data, headers=headers)
+    if response.status_code == 200:
+        user = response.json()
+        expires = datetime.timedelta(seconds=60 * 60*24)
+        access_token = create_access_token(identity=user,expires_delta=expires)
+        return jsonify({"token": access_token, "user_id": user["_id"]})
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+
+# Funcion que se ejecutará siempre de primero antes de que la consulta llegue a la ruta solicitada
+@app.before_request
+def before_request_callback():
+    endPoint = limpiarURL(request.path)
+    excludedRoutes = ["/login"]
+    if excludedRoutes.__contains__(request.path):
+        pass
+    elif verify_jwt_in_request():
+        almacenista = get_jwt_identity()
+        if almacenista["rol"] is not None:
+            tienePersmiso = validarPermiso(endPoint, request.method,almacenista["rol"]["_id"])
+            if not tienePersmiso:
+                return jsonify({"message": "Permission denied1"}), 401
+        else:
+            return jsonify({"message": "Permission denied2"}), 401
+
+
+def limpiarURL(url):
+    partes = url.split("/")
+    for laParte in partes:
+        if re.search('\\d', laParte):
+            url = url.replace(laParte, "?")
+    return url
+
+
+def validarPermiso(endPoint, metodo, idRol):
+    url = dataConfig["url-backend-security"] + "/permisos-roles/validar-permiso/rol/" + str(idRol)
+    tienePermiso = False
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    body = {
+        "url": endPoint,
+        "metodo": metodo
+    }
+    response = requests.get(url, json=body, headers=headers)
+    try:
+        data = response.json()
+        if ("_id" in data):
+            tienePermiso = True
+    except:
+        pass
+    return tienePermiso
+
+############################redireccionamiento###########################
+#############
+@app.route("/almacenista", methods=['GET'])
+def getAlmacenista():
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/almacenista'
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
+
+@app.route("/almacenista", methods=['POST'])
+def crearAlmacenista():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/almacenista'
+    response = requests.post(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+
+@app.route("/almacenista/<string:id>", methods=['GET'])
+def getAlmacenista(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/almacenista/' + id
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
+
+@app.route("/almacenista/<string:id>", methods=['PUT'])
+def modificarAlmacenista(id):
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/almacenista/' + id
+    response = requests.put(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+
+@app.route("/almacenista/<string:id>", methods=['DELETE'])
+def eliminarAlmacenista(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-academic"] + '/almacenista/' + id
+    response = requests.delete(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
+
+@app.route("/", methods=['GET'])
+def test():
+    json = {}
+    json["message"] = "Server running ..."
+    return jsonify(json)
+
+
+def loadFileConfig():
+    with open('config.json') as f:
+        data = json.load(f)
+    return data
+
+
+if __name__ == '__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : " + "http://" + dataConfig["url-backend"] + ":" + str(dataConfig["port"]))
+    serve(app, host=dataConfig["url-backend"], port=dataConfig["port"])
